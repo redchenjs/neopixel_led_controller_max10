94e467c09a15236f95549a88929dae20c2997222
Jack Chen <redchenjs@live.com>
2020-04-05 23:35:32 +0800

    staging 1

--- top.v
+++ top.v
@@ -1,64 +1,58 @@
-module ledtop(
-	inclk,
-	SPI_CLK,
-	data,
-	le,//高电平使能
-	DC,
-	zerodata,
-	spi_write,
-//	rdaddres
+module ws2812_led_controller(
+    input clk_in,
+    input rst_n_in,
+    input spi_sclk_in,
+    input spi_mosi_in,
+    input spi_cs_in,
+    input dc_in,
+    output [7:0] ws2812_out
 );
 
-input inclk,SPI_CLK,data,le,DC;
-output zerodata,spi_write;//,rdaddres,
-
 wire [7:0]outdata,layer;
-wire [7:0]zerodata;
 wire [5:0]wraddress;
 wire [3:0]byte;
-wire locked,outclk,DC_out,read_en,spi_write,transl;
-//wire [5:0]rdaddres;
+wire locked,outclk,dc_out,read_en,spi_write,transl;
 
 supply0 areset;
 
-PLL PLL (
+PLL PLL(
 	.areset(areset),
-	.inclk0(inclk),
+	.inclk0(clk_in),
 	.c0(c0),
-	.locked(locked)
-	);
+	.locked(locked),
+);
 
-globalclk globalclk (
+globalclk globalclk(
 	.inclk(c0),
 	.ena(locked), 
-	.outclk(outclk)
-	);
+	.outclk(outclk),
+);
 /*--------------------------------------------------*/
 SPI_MOSI SPI_MOSI(
 	.SCLK(outclk),//系统时钟，200MHz
-	.SPI_CLK(SPI_CLK),
-	.SPI_DATA(data),
-	.LE(le),//高电平使能
+	.SPI_CLK(spi_sclk_in),
+	.SPI_DATA(spi_mosi_in),
+	.LE(spi_cs_in),//高电平使能
 	.write(spi_write),//RAM写入使能
 	.SHIFT_REG(outdata),
-	.DC_in(DC),//命令数据线
-	.DC_out(DC_out),//命令数据信号锁存
-	.Rst_n(locked)  //全局复位
+	.DC_in(dc_in),//命令数据线
+	.DC_out(dc_out),//命令数据信号锁存
+	.Rst_n(locked),  //全局复位
 );
 RAM_COM_WR RAM_COM_WR(
 	.SCLK(outclk),
 	.Rst_n(locked),  //全局复位
-	.DC(DC_out),//数据命令
+	.DC(dc_out),//数据命令
 	.write(spi_write),//RAM写入使能
 	.SHIFT_REG(outdata),//缓存数据
 	.layer_en(layer),//写入使能
 	.wraddre(wraddress),//地址
 	.byte_en(byte),//字节使能
 	.read(read_en),//读取控制
-	.trans(transl)
+	.trans(transl),
 );
 /*---------------------------------------------------*/
-trans trans0(
+trans layer0(
 	.clk(outclk),		//高速计时时钟
 	.Rst_n(locked),	//全局复位，低电平复位
 	.read(read_en),		//读取触发脉冲
@@ -66,11 +60,10 @@ trans trans0(
 	.byte(byte),
 	.wraddress(wraddress),
 	.wren(layer[0]),
-	.zerodata(zerodata[0]),
+	.zerodata(ws2812_out[0]),
 	.transl(transl),
-//	.addere(rdaddres)//调试用地址显示
 );
-trans trans1(
+trans layer1(
 	.clk(outclk),		//高速计时时钟
 	.Rst_n(locked),	//全局复位，低电平复位
 	.read(read_en),		//读取触发脉冲
@@ -78,9 +71,9 @@ trans trans1(
 	.byte(byte),
 	.wraddress(wraddress),
 	.wren(layer[1]),
-	.zerodata(zerodata[1]),
+	.zerodata(ws2812_out[1]),
 );
-trans trans2(
+trans layer2(
 	.clk(outclk),		//高速计时时钟
 	.Rst_n(locked),	//全局复位，低电平复位
 	.read(read_en),		//读取触发脉冲
@@ -88,9 +81,9 @@ trans trans2(
 	.byte(byte),
 	.wraddress(wraddress),
 	.wren(layer[2]),
-	.zerodata(zerodata[2]),
+	.zerodata(ws2812_out[2]),
 );
-trans trans3(
+trans layer3(
 	.clk(outclk),		//高速计时时钟
 	.Rst_n(locked),	//全局复位，低电平复位
 	.read(read_en),		//读取触发脉冲
@@ -98,9 +91,9 @@ trans trans3(
 	.byte(byte),
 	.wraddress(wraddress),
 	.wren(layer[3]),
-	.zerodata(zerodata[3]),
+	.zerodata(ws2812_out[3]),
 );
-trans trans4(
+trans layer4(
 	.clk(outclk),		//高速计时时钟
 	.Rst_n(locked),	//全局复位，低电平复位
 	.read(read_en),		//读取触发脉冲
@@ -108,9 +101,9 @@ trans trans4(
 	.byte(byte),
 	.wraddress(wraddress),
 	.wren(layer[4]),
-	.zerodata(zerodata[4]),
+	.zerodata(ws2812_out[4]),
 );
-trans trans5(
+trans layer5(
 	.clk(outclk),		//高速计时时钟
 	.Rst_n(locked),	//全局复位，低电平复位
 	.read(read_en),		//读取触发脉冲
@@ -118,9 +111,9 @@ trans trans5(
 	.byte(byte),
 	.wraddress(wraddress),
 	.wren(layer[5]),
-	.zerodata(zerodata[5]),
+	.zerodata(ws2812_out[5]),
 );
-trans trans6(
+trans layer6(
 	.clk(outclk),		//高速计时时钟
 	.Rst_n(locked),	//全局复位，低电平复位
 	.read(read_en),		//读取触发脉冲
@@ -128,9 +121,9 @@ trans trans6(
 	.byte(byte),
 	.wraddress(wraddress),
 	.wren(layer[6]),
-	.zerodata(zerodata[6]),
+	.zerodata(ws2812_out[6]),
 );
-trans trans7(
+trans layer7(
 	.clk(outclk),		//高速计时时钟
 	.Rst_n(locked),	//全局复位，低电平复位
 	.read(read_en),		//读取触发脉冲
@@ -138,7 +131,7 @@ trans trans7(
 	.byte(byte),
 	.wraddress(wraddress),
 	.wren(layer[7]),
-	.zerodata(zerodata[7]),
+	.zerodata(ws2812_out[7]),
 );
 
 endmodule
