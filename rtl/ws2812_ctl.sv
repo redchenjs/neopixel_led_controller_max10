21102a7db2da10fbe8e8fcc6f38b6b0fbbcb30b8
Jack Chen <redchenjs@live.com>
2020-04-13 05:53:38 +0800

    ws2812_ctl: code clean up

--- rtl/ws2812_ctl.sv
+++ rtl/ws2812_ctl.sv
@@ -28,7 +28,6 @@ parameter [1:0] SEND_BIT = 2'b10;   // Send Data Bit
 parameter [1:0] SEND_RST = 2'b11;   // Send Reset Code
 
 logic bit_rdy;
-logic [4:0] bit_sel;
 
 logic ram_rd_st;
 logic ram_rd_en;
@@ -38,6 +37,8 @@ logic [5:0] ram_rd_addr;
 logic [23:0] ram_rd_data;
 
 logic [1:0] ctl_sta;
+
+logic [4:0] bit_sel;
 logic [15:0] rst_cnt;
 
 logic ram_rd_rdy;
@@ -92,11 +93,11 @@ begin
         ctl_sta <= IDLE;
 
         bit_rdy <= 1'b0;
-        bit_sel <= 5'h00;
 
         ram_rd_st <= 1'b0;
         ram_rd_en <= 1'b0;
 
+        bit_sel <= 5'h00;
         rst_cnt <= 16'h0000;
 
         bit_data_out <= 1'b0;
@@ -115,11 +116,11 @@ begin
         endcase
 
         bit_rdy <= (ctl_sta == SEND_BIT) & bit_done;
-        bit_sel <= (ctl_sta == SEND_BIT) ? (bit_sel + bit_done) : 5'h00;
 
         ram_rd_st <= ((ctl_sta == IDLE) | ram_rd_st) & (ctl_sta != SEND_BIT);
         ram_rd_en <= (ctl_sta == READ_RAM) & ~ram_rd_done;
 
+        bit_sel <= (ctl_sta == SEND_BIT) ? (bit_sel + bit_done) : 5'h00;
         rst_cnt <= (ctl_sta == SEND_RST) ? (rst_cnt + 1'b1) : 16'h0000;
 
         bit_data_out <= ram_rd_data[5'd23 - bit_sel];
