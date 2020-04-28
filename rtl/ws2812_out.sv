6ecaac924ec5e4b2b9c7c84663f40048bd6b90f3
Jack Chen <redchenjs@live.com>
2020-04-28 18:53:52 +0800

    ws2812_out: fix apa106 timing issue

--- rtl/ws2812_out.sv
+++ rtl/ws2812_out.sv
@@ -16,27 +16,27 @@ module ws2812_out(
     output logic ws2812_data_out
 );
 
-parameter [7:0] CNT_0_35_US = 2 * 35;
-parameter [7:0] CNT_0_90_US = 2 * 90;
-parameter [7:0] CNT_1_25_US = 2 * 125;
+parameter [8:0] CNT_0_35_US = 2 * 35;
+parameter [8:0] CNT_1_36_US = 2 * 136;
+parameter [8:0] CNT_1_71_US = 2 * 171;
 
 logic bit_bsy;
-logic [7:0] bit_cnt;
+logic [8:0] bit_cnt;
 
 always_ff @(posedge clk_in or negedge rst_n_in)
 begin
     if (!rst_n_in) begin
         bit_bsy <= 1'b0;
-        bit_cnt <= 8'h00;
+        bit_cnt <= 9'h000;
 
         bit_done_out <= 1'b0;
         ws2812_data_out <= 1'b0;
     end else begin
-        bit_bsy <= bit_bsy ? (bit_cnt != CNT_1_25_US) : bit_rdy_in;
-        bit_cnt <= bit_bsy ? bit_cnt + 1'b1 : 8'h00;
+        bit_bsy <= bit_bsy ? (bit_cnt != CNT_1_71_US) : bit_rdy_in;
+        bit_cnt <= bit_bsy ? bit_cnt + 1'b1 : 9'h000;
 
-        bit_done_out <= bit_bsy & (bit_cnt == CNT_1_25_US);
-        ws2812_data_out <= bit_bsy & ((bit_data_in & (bit_cnt < CNT_0_90_US))
+        bit_done_out <= bit_bsy & (bit_cnt == CNT_1_71_US);
+        ws2812_data_out <= bit_bsy & ((bit_data_in & (bit_cnt < CNT_1_36_US))
                                    | (~bit_data_in & (bit_cnt < CNT_0_35_US)));
     end
 end
